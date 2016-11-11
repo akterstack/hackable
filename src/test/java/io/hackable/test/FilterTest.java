6f8b136feb5cec30ffcadf7a08ac57fa79e29609
jRoadie <akter.hossain.ict@gmail.com>
2016-11-11 23:28:48 +0600

    refactoring...

--- src/test/java/io/hackable/test/FilterTest.java
+++ src/test/java/io/hackable/test/FilterTest.java
@@ -8,13 +8,13 @@ public class FilterTest {
 
     @Test
     public void testApplyFilter() {
-        onFilter("init", this.getClass(), (FilterMock mock) -> new FilterMock("Akter " + mock.getName()));
+        onFilter("init", (FilterMock mock) -> new FilterMock("Akter " + mock.getName()));
         FilterMock filterMock = applyFilter("init", new FilterMock("Sohag"));
         System.out.println(filterMock);
     }
 
     static {
-        onFilter("init", FilterTest.class, (FilterMock mock) -> new FilterMock("Hossain " + mock.getName()));
+        onFilter("init", (FilterMock mock) -> new FilterMock("Hossain " + mock.getName()));
     }
 
 }
