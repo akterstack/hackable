8ad6eadaea4cd90216d162e890754772756cf660
shg@dsi <akter.hossain@dsinnovators.com>
2017-08-18 15:05:53 +0600

    updating...

--- src/main/java/io/hackable/Hackable.java
+++ src/main/java/io/hackable/Hackable.java
@@ -5,78 +5,62 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public final class Hackable {
 
-    private static final Map<String, List<Handler<?>>> eventHandlersMap = new HashMap<>();
-    private static final Map<String, List<Filter<?>>> filterHandlersMap = new HashMap<>();
+    private enum Context {BEFORE, AFTER}
+
+    private static final Map<String, List<Handler<?>>> actionHandlersHostMap = new HashMap<>();
+    private static final Map<String, List<Filter<?>>> filterHandlersHostMap = new HashMap<>();
 
     private Hackable() {}
 
-    public static <T> void on(String eventName, Handler<T> eventHandler) {
-        _on(resolveHackableHandlerKey(eventName, null), eventHandler);
+    public static <T> void before(String actionName, Handler<T> actionHandler) {
+        doAction(Context.BEFORE, actionName, actionHandler);
     }
 
-    public static <T> void on(String eventName, Class contextClass, Handler<T> eventHandler) {
-        _on(resolveHackableHandlerKey(eventName, contextClass), eventHandler);
+    public static <T> void after(String actionName, Handler<T> actionHandler) {
+        doAction(Context.AFTER, actionName, actionHandler);
     }
 
-    private static <T> void _on(String eventHostKey, Handler<T> eventHandler) {
-        List<Handler<?>> existingHandlers = eventHandlersMap.get(eventHostKey);
-        if (existingHandlers == null) {
-            existingHandlers = new ArrayList<>();
-            eventHandlersMap.put(eventHostKey, existingHandlers);
-        }
-        existingHandlers.add(eventHandler);
+    /* alias method of after() */
+    public static <T> void on(String actionName, Handler<T> actionHandler) {
+        after(actionName, actionHandler);
     }
 
-    public static <T> void trigger(String eventName, T eventData) {
-        _trigger(resolveHackableHandlerKey(eventName, null), eventData);
+    private static <T> void doAction(Context context, String actionName, Handler<T> actionHandler) {
+        List<Handler<?>> existingHandlers =
+                actionHandlersHostMap
+                        .computeIfAbsent(resolveActionHostKey(context, actionName), k -> new ArrayList<>());
+        existingHandlers.add(actionHandler);
     }
 
-    public static <T> void trigger(String eventName, Class contextClass, T eventData) {
-        _trigger(resolveHackableHandlerKey(eventName, contextClass), eventData);
+    public static <T> void trigger(String actionName, T actionData) {
+        trigger(Context.AFTER, actionName, actionData);
     }
 
-    private static <T> void _trigger(String eventHostKey, T eventData) {
-        List<Handler<?>> consumers = eventHandlersMap.get(eventHostKey);
-        if (consumers != null) {
-            for (Handler<?> consumer : consumers) {
-                ((Handler<T>)consumer).handle(eventData);
+    public static <T> void trigger(Context context, String actionName, T actionData) {
+        List<Handler<?>> actions = actionHandlersHostMap.get(resolveActionHostKey(context, actionName));
+        if(actions != null) {
+            for(Handler action : actions) {
+                action.handle(actionData);
             }
         }
     }
 
     public static <R> void onFilter(String filterName, Filter<R> filterHandler) {
-        _onFilter(resolveHackableHandlerKey(filterName, null), filterHandler);
-    }
-
-    public static <R> void onFilter(String filterName, Class contextClass, Filter<R> filterHandler) {
-        _onFilter(resolveHackableHandlerKey(filterName, contextClass), filterHandler);
-    }
-
-    private static <R> void _onFilter(String filterHostKey, Filter<R> filterHandler) {
-        List<Filter<?>> existingHandlers = filterHandlersMap.get(filterHostKey);
-        if (existingHandlers == null) {
-            existingHandlers = new LinkedList<>();
-            filterHandlersMap.put(filterHostKey, existingHandlers);
-        }
+        String filterHostKey = filterName.toLowerCase();
+        List<Filter<?>> existingHandlers =
+                filterHandlersHostMap.computeIfAbsent(filterHostKey, k -> new LinkedList<>());
         existingHandlers.add(filterHandler);
     }
 
-    public static <R> R applyFilter(String filterName, R filterableObject) {
-        return _applyFilter(resolveHackableHandlerKey(filterName, null), filterableObject);
-    }
-
-    public static <R> R applyFilter(String filterName, Class contextClass, R filterableObject) {
-        return _applyFilter(resolveHackableHandlerKey(filterName, contextClass), filterableObject);
-    }
-
-    private static <R> R _applyFilter(String filterHostKey, R filterableObject) {
-        List<Filter<?>> filters = filterHandlersMap.get(filterHostKey);
-        if (filters != null) {
+    public static <R> R filter(String filterName, R filterableObject) {
+        List<Filter<?>> filters = filterHandlersHostMap.get(filterName.toLowerCase());
+        if(filters != null) {
             R fobj = filterableObject;
-            for (Filter f : filters) {
+            for(Filter f : filters) {
                 fobj = (R)f.filter(fobj);
             }
             return fobj;
@@ -84,8 +68,15 @@ public final class Hackable {
         return filterableObject;
     }
 
-    private static String resolveHackableHandlerKey(String name, Class contextClass) {
-        return (contextClass != null ? contextClass.getCanonicalName() : "") + ":" + name;
+    private static String resolveActionHostKey(Context context, String actionName) {
+        return context.name().toLowerCase() + ":" + actionName.toLowerCase();
+    }
+
+    public static <T> T hack(String actionName, T actionData, Supplier<T> supplier) {
+        Hackable.trigger(Context.BEFORE, actionName, actionData);
+        T result = supplier.get();
+        Hackable.trigger(Context.AFTER, actionName, result);
+        return result;
     }
 
 }
--- src/test/java/io/hackable/test/FilterTest.java
+++ src/test/java/io/hackable/test/FilterTest.java
@@ -9,7 +9,7 @@ public class FilterTest {
     @Test
     public void testApplyFilter() {
         onFilter("init", (FilterMock mock) -> new FilterMock("Akter " + mock.getName()));
-        FilterMock filterMock = applyFilter("init", new FilterMock("Sohag"));
+        FilterMock filterMock = filter("init", new FilterMock("Sohag"));
         System.out.println(filterMock);
     }
 
