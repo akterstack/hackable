9b8a4fa6dcdd3e398f505582d4d1150e0f206e94
shg@dsi <akter.hossain@dsinnovators.com>
2017-08-21 12:14:38 +0600

    refactoring

--- src/main/java/io/hackable/Hackable.java
+++ src/main/java/io/hackable/Hackable.java
@@ -9,74 +9,86 @@ import java.util.function.Supplier;
 
 public final class Hackable {
 
-    private enum Context {BEFORE, AFTER}
+  private enum Context {BEFORE, AFTER}
 
-    private static final Map<String, List<Handler<?>>> actionHandlersHostMap = new HashMap<>();
-    private static final Map<String, List<Filter<?>>> filterHandlersHostMap = new HashMap<>();
+  private static final Map<String, List<Handler<?>>> actionHandlersHostMap = new HashMap<>();
+  private static final Map<String, List<Filter<?>>> filterHandlersHostMap = new HashMap<>();
 
-    private Hackable() {}
+  private Hackable() {}
 
-    public static <T> void before(String actionName, Handler<T> actionHandler) {
-        doAction(Context.BEFORE, actionName, actionHandler);
+  public static <T> void before(String actionName, Handler<T> actionHandler) {
+    registerActionHandler(Context.BEFORE, actionName, actionHandler);
+  }
+
+  public static <T> void after(String actionName, Handler<T> actionHandler) {
+    registerActionHandler(Context.AFTER, actionName, actionHandler);
+  }
+
+  /* alias method of after() */
+  public static <T> void on(String actionName, Handler<T> actionHandler) {
+    after(actionName, actionHandler);
+  }
+
+  private static <T> void registerActionHandler(Context context, String actionName, Handler<T> actionHandler) {
+    List<Handler<?>> existingHandlers =
+        actionHandlersHostMap
+            .computeIfAbsent(resolveActionHostKey(context, actionName), k -> new ArrayList<>());
+    existingHandlers.add(actionHandler);
+  }
+
+  public static <T> void trigger(String actionName, T actionData) {
+    trigger(Context.AFTER, actionName, actionData);
+  }
+
+  public static <T> void trigger(Context context, String actionName, T actionData) {
+    doAction(context, actionName, actionData);
+  }
+
+  private static <T> void doAction(Context context, String actionName, T actionData) {
+    List<Handler<?>> actions = actionHandlersHostMap.get(resolveActionHostKey(context, actionName));
+    if(actions != null) {
+      for(Handler action : actions) {
+        action.handle(actionData);
+      }
     }
+  }
 
-    public static <T> void after(String actionName, Handler<T> actionHandler) {
-        doAction(Context.AFTER, actionName, actionHandler);
-    }
+  public static <R> void onFilter(String filterName, Filter<R> filterHandler) {
+    registerFilterHandler(filterName, filterHandler);
+  }
 
-    /* alias method of after() */
-    public static <T> void on(String actionName, Handler<T> actionHandler) {
-        after(actionName, actionHandler);
-    }
+  private static <R> void registerFilterHandler(String filterName, Filter<R> filterHandler) {
+    String filterHostKey = filterName.toLowerCase();
+    List<Filter<?>> existingHandlers =
+        filterHandlersHostMap.computeIfAbsent(filterHostKey, k -> new LinkedList<>());
+    existingHandlers.add(filterHandler);
+  }
 
-    private static <T> void doAction(Context context, String actionName, Handler<T> actionHandler) {
-        List<Handler<?>> existingHandlers =
-                actionHandlersHostMap
-                        .computeIfAbsent(resolveActionHostKey(context, actionName), k -> new ArrayList<>());
-        existingHandlers.add(actionHandler);
-    }
+  public static <R> R filter(String filterName, R filterableObject) {
+    return applyFilter(filterName, filterableObject);
+  }
 
-    public static <T> void trigger(String actionName, T actionData) {
-        trigger(Context.AFTER, actionName, actionData);
+  private static <R> R applyFilter(String filterName, R filterableObject) {
+    List<Filter<?>> filters = filterHandlersHostMap.get(filterName.toLowerCase());
+    if(filters != null) {
+      R fobj = filterableObject;
+      for(Filter f : filters) {
+        fobj = (R)f.filter(fobj);
+      }
+      return fobj;
     }
+    return filterableObject;
+  }
 
-    public static <T> void trigger(Context context, String actionName, T actionData) {
-        List<Handler<?>> actions = actionHandlersHostMap.get(resolveActionHostKey(context, actionName));
-        if(actions != null) {
-            for(Handler action : actions) {
-                action.handle(actionData);
-            }
-        }
-    }
+  private static String resolveActionHostKey(Context context, String actionName) {
+    return context.name().toLowerCase() + ":" + actionName.toLowerCase();
+  }
 
-    public static <R> void onFilter(String filterName, Filter<R> filterHandler) {
-        String filterHostKey = filterName.toLowerCase();
-        List<Filter<?>> existingHandlers =
-                filterHandlersHostMap.computeIfAbsent(filterHostKey, k -> new LinkedList<>());
-        existingHandlers.add(filterHandler);
-    }
-
-    public static <R> R filter(String filterName, R filterableObject) {
-        List<Filter<?>> filters = filterHandlersHostMap.get(filterName.toLowerCase());
-        if(filters != null) {
-            R fobj = filterableObject;
-            for(Filter f : filters) {
-                fobj = (R)f.filter(fobj);
-            }
-            return fobj;
-        }
-        return filterableObject;
-    }
-
-    private static String resolveActionHostKey(Context context, String actionName) {
-        return context.name().toLowerCase() + ":" + actionName.toLowerCase();
-    }
-
-    public static <T> T hack(String actionName, T actionData, Supplier<T> supplier) {
-        Hackable.trigger(Context.BEFORE, actionName, actionData);
-        T result = supplier.get();
-        Hackable.trigger(Context.AFTER, actionName, result);
-        return result;
-    }
+  public static <T> T hack(String actionName, T actionData, Supplier<T> supplier) {
+    Hackable.trigger(Context.BEFORE, actionName, actionData);
+    T result = supplier.get();
+    Hackable.trigger(Context.AFTER, actionName, result);
+    return result;
+  }
 
 }
